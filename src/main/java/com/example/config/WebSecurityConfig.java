708b1075115701a5606d650d1e3d7f4fdc2f62c6
Emmanuel Catunda Freitas <emmanuelcatunda@gmail.com>
2018-04-01 23:46:15 -0400

    initial commit

---
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+
+@Configuration
+@ComponentScan(basePackages={"com.example"})
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().defaultSuccessUrl("/index.jsp",true);
+	}
+
+@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.ldapAuthentication()
+				.userDnPatterns("uid={0}")
+                .contextSource(contextSource())
+                .and()
+                .inMemoryAuthentication()
+                .withUser("admin").roles("ADMIN")
+                .password("{noop}admin");		
+	}
+
+
+	@Bean
+    public DefaultSpringSecurityContextSource contextSource() {
+        return  new DefaultSpringSecurityContextSource(
+                Collections.singletonList("ldap://ldap.forumsys.com:389"), "dc=example,dc=com");     
+    }
+	
+}
---
+++ src/main/webapp/WEB-INF/jsp/admin.jsp
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Spring Boot Example</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="sect1">
+<h2 id="_http_booster">Spring boot example</h2>
+<div class="sectionbody">
+<div class="paragraph">
+<p>An example of a simple Spring Boot application running on CodeEnvy with eclipse CHE</p>
+</div>
+</div>
+</div>
+<p class="lead">Users</p>
+<ul>
+<li><em>riemann</em></li>
+<li><em>gauss</em></li>
+<li><em>euler</em></li>
+<li><em>euclid</em></li>
+<li><em>einstein</em></li>
+<li><em>newton</em></li>
+<li><em>galieleo</em></li>
+<li><em>tesla</em></li>
+</ul>
+</div>
+</body>
+</html>
